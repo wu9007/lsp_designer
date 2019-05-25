0f79d3dc5e0123e825a03baea6f11bc262ea83d6
leyan95 <18231926271@163.com>
2019-05-25 13:27:58 +0800

    update.

--- lib/Selector.dart
+++ lib/Selector.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 /// 下拉列表
 /// Created by Shusheng.
 class Selector<T> extends StatefulWidget {
-  Selector({@required this.value,
-    @required this.label,
-    @required this.hint,
-    @required this.store,
-    @required this.onChange,
-    this.disabled = false})
+  Selector(
+      {@required this.value,
+        @required this.label,
+        @required this.hint,
+        @required this.store,
+        @required this.onChange,
+        this.disabled = false})
       : assert(store == null ||
       store.isEmpty ||
       value == null ||
@@ -31,6 +32,13 @@ class Selector<T> extends StatefulWidget {
 class SelectorState<T> extends State<Selector<T>> {
   @override
   Widget build(BuildContext context) {
+    dynamic disabledHint;
+    if (widget.value != null &&
+        widget.store != null &&
+        widget.store.length > 0) {
+      disabledHint =
+          widget.store.singleWhere((item) => item.value == widget.value)?.child;
+    }
     return Container(
       margin: EdgeInsets.symmetric(vertical: 3.0, horizontal: 8.0),
       child: Row(
@@ -43,12 +51,13 @@ class SelectorState<T> extends State<Selector<T>> {
                 child: DropdownButton(
                   iconDisabledColor: Colors.black54,
                   iconEnabledColor: Colors.black54,
+                  disabledHint: disabledHint,
                   hint: Container(
                     child: widget.hint,
                   ),
                   value: widget.value,
                   items: widget.store,
-                  onChanged: widget.disabled ? (value) => {} : widget.onChange,
+                  onChanged: widget.disabled ? null : widget.onChange,
                   isExpanded: true,
                   iconSize: 35,
                 ),
